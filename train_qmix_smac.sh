a796ddb7234f2b815d2600a84aa11b692194c422
anynomous99 <nn243549184@outlook.com>
2021-06-04 18:36:15 +0100

    updata path

--- train_qmix_smac.sh
+++ train_qmix_smac.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 coma_X/src/main.py --config=qmix --env-config=sc2 with env_args.map_name=6h_vs_8z
+python3 coma_ob/src/main.py --config=qmix --env-config=sc2 with env_args.map_name=6h_vs_8z
